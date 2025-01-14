30d3eb13dad135cfde859539d2ad90f45b7f0765
Ali Samhat <alisamhat2007@gmail.com>
2025-01-14 22:44:09 +0100

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <div className="text-red-800">Hello World</div>
+      <Outlet />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App.jsx";
+import HomePage from "./pages/HomePage.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<HomePage />} index={true} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
