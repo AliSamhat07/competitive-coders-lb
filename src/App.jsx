2d8f28134f83f6aaa877cb45fb406dfd0b7e91bd
Ali Samhat <alisamhat2007@gmail.com>
2025-01-16 10:38:44 +0100

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div className="font-poppins bg-accent-beige min-h-screen">
       <Outlet />
-    </>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "accent-red": "#cf4657",
+        "accent-green": "#297752",
+        "accent-beige": "#FDFDEF",
+      },
+    },
+
+    fontFamily: {
+      poppins: "Poppins",
+    },
   },
   plugins: [],
 };
