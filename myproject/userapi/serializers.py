d81f8d8252db0cc0b1ce92e394ae429fb44717b4
abbassthe <47321004+abbassthe@users.noreply.github.com>
2025-01-20 16:59:00 +0200

    host

--- myproject/userapi/serializers.py
+++ myproject/userapi/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
 from .models import UserRegistration
 
+from rest_framework import serializers
+from .models import User  # Import your User model or a custom model if applicable
+
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserRegistration
-        fields = '__all__'
+        model = User
+        fields = [
+            "address",
+            "fullname",
+            "email",
+            "phone",
+            "high_school",
+            "middle_school",
+        ]
+        extra_kwargs = {
+            "email": {"required": True},
+            "fullname": {"required": True},
+            "phone": {"required": True},
+            "high_school": {"required": True},
+            "middle_school": {"required": True},
+            "address": {"required": True},
+        }
+
+    def validate_phone(self, value):
+        if len(value) < 10 or not value.isdigit():
+            raise serializers.ValidationError(
+                "Phone number must be at least 10 digits."
+            )
+        return value
